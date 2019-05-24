0e191a898a88d0e20acacc1fea62ff34ca4d5170
Shuge Lee <shuge.lee@gmail.com>
2019-05-24 12:08:45 +0800

    Refactoring.

--- examples/json.redis/main.go
+++ examples/json.redis/main.go
@@ -5,25 +5,33 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
 
 	"github.com/OpenCCTV/redis/redis"
 )
 
 var (
-	Addr     = flag.String("addr", "127.0.0.1:6379", "redis instance address")
-	Password = flag.String("auth", "", "redis instance passsword")
+	Addr     string
+	Password string
 )
 
 func main() {
+	log.SetFlags(log.Ldate | log.Ltime | log.Llongfile)
+
+	flag.StringVar(&Addr, "addr", "127.0.0.1:6379", "redis instance address")
+	flag.StringVar(&Password, "auth", "", "redis instance passsword")
+
 	flag.Parse()
-	metrics, err := metricsRedis.Gets(*Addr, *Password)
+
+	metrics, err := metricsRedis.Gets(Addr, Password)
 	if err != nil {
-		panic(err)
+		log.Fatalln(err)
 	}
 
 	out, err := json.MarshalIndent(metrics, "", "  ")
 	if err != nil {
-		panic(err)
+		log.Fatalln(err)
 	}
+
 	fmt.Println(string(out))
 }
--- redis/collector.go
+++ redis/collector.go
@@ -21,6 +21,7 @@ func Gets(addr, password string) (*map[string]interface{}, error) {
 		return &result, err
 	}
 	defer conn.Close()
+	result["ping"] = PingUp
 
 	client := redis.NewClient(&redis.Options{
 		Addr:     addr,
@@ -32,6 +33,8 @@ func Gets(addr, password string) (*map[string]interface{}, error) {
 	// redis ping-pong test
 	pong, err := client.Ping().Result()
 	if err != nil {
+		log.Println("net.DialTimeout", addr, password, err)
+
 		errStr := strings.ToLower(err.Error())
 		if strings.Index(errStr, "but no password is set") != -1 {
 			// walk around issue redis raise error if you send auth password but it not required
